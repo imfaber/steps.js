6c617ab79e616734c38e75e3fd653880fa083d1d
Fabrizio Meinero <imfaberdev@gmail.com>
2018-06-23 22:35:33 +0200

    Replace postcss with node-sass

--- rollup.config.js
+++ rollup.config.js
@@ -2,34 +2,12 @@ import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 import istanbul from 'rollup-plugin-istanbul';
 import {uglify}  from 'rollup-plugin-uglify';
-import sass from 'node-sass'
-import autoprefixer from 'autoprefixer'
-import cssnano from 'cssnano'
-import postcss from 'rollup-plugin-postcss'
 
 let pkg = require('./package.json');
 let outputFile = pkg.main;
 
 // The plugin Array.
 let plugins = [];
-
-// Add postcss to plugins.
-let postcssOptions = {
-  preprocessor: (content, id) => new Promise((resolve, reject) => {
-    const result = sass.renderSync({file: id});
-    resolve({code: result.css.toString()});
-  }),
-  plugins:      [
-    autoprefixer
-  ],
-  sourceMap:    (process.env.BUILD !== 'production'),
-  extract:      true,
-  extensions:   ['.scss', '.css']
-};
-if (process.env.BUILD === 'production') {
-  postcssOptions.plugins.push(cssnano);
-}
-plugins.push(postcss(postcssOptions));
 
 // Add babel.
 plugins.push(babel(babelrc()));
